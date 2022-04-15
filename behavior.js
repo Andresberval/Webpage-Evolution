1d4f5728859f6ef9848695727621c8b3ae27c318
Andrés Felipe <76921745+Andresberval@users.noreply.github.com>
2022-04-15 16:26:16 -0500

    Add files via upload

---
+++ behavior.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.getElementById("smart_thumbnail");
+    
+    var thumbnailElement = document.getElementById("smart_thumbnail");
+    thumbnailElement.addEventListener("click", function() {
+    thumbnailElement.className = "small";    
+
+    });
+
+});
